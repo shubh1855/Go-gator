6a8dc2ad7b5f99a1635f5cc1662af9d4342e9491
shubh1855 <paulshubhranil1855@gmail.com>
2026-05-24 10:25:23 +0530

    feat(db): add users table migration

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+Create TABLE users (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name TEXT NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
